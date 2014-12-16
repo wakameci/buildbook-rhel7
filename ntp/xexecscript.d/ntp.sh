6fc93605166d2c7373ea424379da9a7417271ff1
Masahito Yoshida <masahito@axsh.net>
2014-12-17 02:12:10 +0900

    add ntp.

---
+++ ntp/xexecscript.d/ntp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+
+declare chroot_dir=$1
+
+chroot $1 $SHELL -ex <<'EOS'
+  yum install --disablerepo=updates -y ntp ntpdate
+
+ #chkconfig ntpd    on
+ #chkconfig ntpdate on
+EOS
