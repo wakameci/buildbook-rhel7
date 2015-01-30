947a7827d26f7adec3fd09df3a89053b6c5c00d0
Masahito Yoshida <masahito@axsh.net>
2015-01-30 17:27:59 +0900

    organize glibc dependency packages.

--- jenkins.core/xexecscript.d/build-tools.sh
+++ jenkins.core/xexecscript.d/build-tools.sh
@@ -19,11 +19,19 @@ chroot $1 $SHELL -ex <<'EOS'
     openssl-devel zlib-devel readline-devel
 EOS
 
-# "gcc" depends on glibc* which is pached in "ghost-buster".
+# "gcc"/"gcc-c++" depends on glibc* which is pached in "ghost-buster".
 #
 # 7.0.1406)
 #   "ghost-buster" updates glibc-* using updates yum repository.
 #   because gcc must not disable updates yum repository.
+#
+# Error: Package: gcc-c++-4.8.2-16.el7.x86_64 (base)
+#            Requires: gcc = 4.8.2-16.el7
+#            Installed: gcc-4.8.2-16.2.el7_0.x86_64 (@updates)
+#                gcc = 4.8.2-16.2.el7_0
+#            Available: gcc-4.8.2-16.el7.x86_64 (base)
+#                gcc = 4.8.2-16.el7
+#
 
 chroot $1 $SHELL -ex <<'EOS'
   releasever=$(< /etc/yum/vars/releasever)
@@ -31,16 +39,13 @@ chroot $1 $SHELL -ex <<'EOS'
   case "${releasever}" in
     7.0.1406)
       yum install -y \
-        gcc
+        gcc \
+        gcc-c++
       ;;
     *)
       yum install --disablerepo=updates -y \
-        gcc
+        gcc \
+        gcc-c++
       ;;
   esac
-EOS
-
-chroot $1 $SHELL -ex <<'EOS'
-  yum install --disablerepo=updates -y \
-    gcc-c++
 EOS
