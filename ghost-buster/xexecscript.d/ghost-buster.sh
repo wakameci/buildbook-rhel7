6c1d23d645060990c1b814d8e2a167cc13ae4b07
Masahito Yoshida <masahito@axsh.net>
2015-01-30 16:52:21 +0900

    add ghost-buster.

---
+++ ghost-buster/xexecscript.d/ghost-buster.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+set -o pipefail
+
+declare chroot_dir=$1
+
+chroot $1 $SHELL -ex <<'EOS'
+  releasever=$(< /etc/yum/vars/releasever)
+
+  arch=$(arch)
+  case "${arch}" in
+    i*86)   basearch=i386 arch=i686 ;;
+    x86_64) basearch=${arch} ;;
+  esac
+
+  case "${releasever}" in
+    7.0.1406)
+      yum update  -y \
+        glibc \
+        glibc-common \
+        glibc-devel \
+        glibc-headers
+      ;;
+  esac
+EOS
