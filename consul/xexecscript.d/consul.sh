ab2fcbfce26616f5bc00be16c9269e919053bfb6
Masahito Yoshida <masahito@axsh.net>
2015-02-23 15:46:40 +0900

    add consul.

---
+++ consul/xexecscript.d/consul.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+set -o pipefail
+
+declare chroot_dir=$1
+
+chroot $1 $SHELL -ex <<'EOS'
+  yum install --disablerepo=updates -y curl unzip
+EOS
+
+chroot $1 $SHELL -ex <<'EOS'
+  # Download and install Serf
+  cd /tmp
+  case "$(arch)" in
+  x86_64) arch=amd64 ;;
+    i*86) arch=386   ;;
+  esac
+
+  until curl -fsSkL -o consul.zip https://dl.bintray.com/mitchellh/consul/0.5.0_linux_${arch}.zip; do
+    sleep 1
+  done
+  unzip consul.zip
+  mv consul /usr/local/bin/consul
+  rm -f consul.zip
+EOS
